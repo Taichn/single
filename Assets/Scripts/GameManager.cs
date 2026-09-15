1825e4a39dcae78666ec86a0d2d3ffd75d0dc608
Taichn <kmm2559373@stu.o-hara.ac.jp>
2026-09-15 16:52:55 +0900

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,38 +1,52 @@
 using UnityEngine;
-using TMPro;
 using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    public int correctAnswer = 2;
-    public GameObject resultText;
-    public TMP_Text resultTextComponent;
+    public int correctAnswer = 4;
+
+    public GameObject resultImage;
+    public GameObject tutorialResult;
+    public GameObject wrongImage;
 
     public void SelectAnswer(int answerNumber)
     {
-        // すでに処理中なら無視
         StopAllCoroutines();
 
         if (answerNumber == correctAnswer)
         {
-            resultText.SetActive(true);
-            resultTextComponent.text = "正解！";
+            // 正解！
+            resultImage.SetActive(true);
+            wrongImage.SetActive(false);
+
+            StartCoroutine(ShowTutorialResult());
         }
         else
         {
-            resultText.SetActive(true);
-            resultTextComponent.text = "不正解！";
+            // 不正解
+            wrongImage.SetActive(true);
 
-            StartCoroutine(HideResultAfterDelay());
+            StartCoroutine(HideWrongAfterDelay());
         }
     }
 
-    IEnumerator HideResultAfterDelay()
+    IEnumerator ShowTutorialResult()
     {
-        // 1秒待つ
+        // 正解！を1秒表示
         yield return new WaitForSeconds(1f);
 
-        // 結果表示を消す
-        resultText.SetActive(false);
+        // 正解！を消す
+        resultImage.SetActive(false);
+
+        // リザルト画面を表示
+        tutorialResult.SetActive(true);
+    }
+
+    IEnumerator HideWrongAfterDelay()
+    {
+        // 不正解を1秒表示
+        yield return new WaitForSeconds(1f);
+
+        wrongImage.SetActive(false);
     }
 }
